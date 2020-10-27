bad0c33a3fcf1a9337fa2db1f96216d5eaf2e405
yuyu890924 <208410232@gms.tku.edu.tw>
2020-10-27 18:58:01 +0800

    Updates

---
+++ precache-manifest.244dedca9ff60fa645b7a9e31e05e583.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bf8024b5e5c01f9feb2fd7a8d46ad7",
+    "url": "/2N-mid-208410232-project/index.html"
+  },
+  {
+    "revision": "2e2be30fc49badcec002",
+    "url": "/2N-mid-208410232-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1918756b38eebac85b53",
+    "url": "/2N-mid-208410232-project/static/css/main.8cfef1d4.chunk.css"
+  },
+  {
+    "revision": "2e2be30fc49badcec002",
+    "url": "/2N-mid-208410232-project/static/js/2.6aefec96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2N-mid-208410232-project/static/js/2.6aefec96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1918756b38eebac85b53",
+    "url": "/2N-mid-208410232-project/static/js/main.81f46738.chunk.js"
+  },
+  {
+    "revision": "fc69e79ffd2df7d32501",
+    "url": "/2N-mid-208410232-project/static/js/runtime-main.3a10e95c.js"
+  },
+  {
+    "revision": "fc27e1674f5b3d1c1377f5b418c86171",
+    "url": "/2N-mid-208410232-project/static/media/1.fc27e167.JPG"
+  },
+  {
+    "revision": "20ed9eed5d284595599578747659334d",
+    "url": "/2N-mid-208410232-project/static/media/10.20ed9eed.PNG"
+  },
+  {
+    "revision": "bd4f69e8677c5f12f7eee8d63f26f83a",
+    "url": "/2N-mid-208410232-project/static/media/2.bd4f69e8.JPG"
+  },
+  {
+    "revision": "142e4ef108702eebcb408a222883369b",
+    "url": "/2N-mid-208410232-project/static/media/3.142e4ef1.JPG"
+  },
+  {
+    "revision": "20a6c58ad99a12de80d61d7d1fd81736",
+    "url": "/2N-mid-208410232-project/static/media/4.20a6c58a.JPG"
+  },
+  {
+    "revision": "922fc763390d3712c077106aaa0b54a2",
+    "url": "/2N-mid-208410232-project/static/media/5.922fc763.JPG"
+  },
+  {
+    "revision": "f0ef7ac386267215bb704246e4e23411",
+    "url": "/2N-mid-208410232-project/static/media/6.f0ef7ac3.JPG"
+  },
+  {
+    "revision": "b6c7e93db5e1bcc45913636fdf5b2fdd",
+    "url": "/2N-mid-208410232-project/static/media/7.b6c7e93d.JPG"
+  },
+  {
+    "revision": "fe7f7625c17ece5c41050a3c754c0360",
+    "url": "/2N-mid-208410232-project/static/media/8.fe7f7625.JPG"
+  },
+  {
+    "revision": "f7a90d481e02af9b82e6025b124118c5",
+    "url": "/2N-mid-208410232-project/static/media/9.f7a90d48.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7070346ebc8ba7972bd26dc96463e21.js"
+  "/2N-mid-208410232-project/precache-manifest.244dedca9ff60fa645b7a9e31e05e583.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2N-mid-208410232-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
